f440e8aef7c0fec4b3c997aa7146271b1e42f6c0
James Mead <james@floehopper.org>
2018-05-05 16:36:01 +0100

    Add script to hide private bookmarks on Pinboard

---
+++ Hide private Pinboard bookmarks.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Hide private Pinboard bookmarks
+// @namespace    http://jamesmead.org/
+// @version      0.1
+// @description  Hide private Pinboard bookmarks by default
+// @author       James Mead
+// @match        https://pinboard.in/u:floehopper*
+// @exclude      https://pinboard.in/u:floehopper/private/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var bookmarks = document.getElementsByClassName('bookmark private');
+    for(var i = 0; i < bookmarks.length; i++) {
+      bookmarks.item(i).style.display = 'none';
+    }
+    console.log('Private bookmarks hidden');
+})();
