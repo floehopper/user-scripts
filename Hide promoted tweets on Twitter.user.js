97de469b153b7369b5c8187635705446669b20f3
James Mead <james@floehopper.org>
2018-05-05 16:35:29 +0100

    Add script to hide promoted tweets on Twitter

---
+++ Hide promoted tweets on Twitter.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Hide promoted tweets on Twitter
+// @namespace    http://jamesmead.org/
+// @version      0.1
+// @description  Hide the annoying promoted tweets on Twitter
+// @author       James Mead
+// @match        https://twitter.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('span.Icon--promoted').closest('.tweet').hide();
+})();
