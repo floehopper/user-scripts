5fbde7a304b20b8e01356edc0c51008485a34ac3
James Mead <james@floehopper.org>
2025-12-07 17:32:32 +0000

    Add user preferences to disable ML features
    Disable various machine learning features in Firefox.

---
+++ user.js
@@ -0,0 +1,14 @@
+// Add this in the relevant Firefox profile directory
+// ref: https://social.treehouse.systems/@dysfun/115021261579545215
+
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.chat.shortcuts", false);
+user_pref("browser.ml.chat.shortcuts.custom", false);
+user_pref("browser.ml.chat.sidebar", false);
+user_pref("browser.ml.enable", false);
+
+user_pref("extensions.ml.enabled", false);
+
+user_pref("browser.tabs.groups.smart.enabled", false);
+user_pref("browser.tabs.groups.smart.optin", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
